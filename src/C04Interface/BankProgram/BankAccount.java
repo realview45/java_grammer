ab98d8630581237ff72c7dddd64afab31bb30b38
realview45 <kjink0125@gmail.com>
2025-12-21 20:18:07 +0900

    BankService

--- src/C04Interface/BankProgram/BankAccount.java
+++ src/C04Interface/BankProgram/BankAccount.java
@@ -2,4 +2,39 @@ package C04Interface.BankProgram;
 //사용자와의 인터페이스를 담당하는 프로그램 계층
 //엔티티계층으로서 객체에 대한 상태값을 관리
 public class BankAccount {
+    private String name;
+    private String accountNumber;
+    private long balance;
+
+    public BankAccount(String name, String accountNumber, long balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //    최종 금액을 update : 아래 메서드를 통해서 입/출금 모두에 사용.
+    // balance가 private이므로 update작업만 엔티티계층에서 수행
+    public void updateBalance(long balance){
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
--- src/C04Interface/BankProgram/BankService.java
+++ src/C04Interface/BankProgram/BankService.java
@@ -1,4 +1,49 @@
 package C04Interface.BankProgram;
 //핵심로직(잔고검증, 금액합산 등)을 수행하는 프로그램 계층
 public class BankService {
+//    계좌조회
+    public void inquiry(BankAccount ba){
+        String name = ba.getName();
+        String accountNumber = ba.getAccountNumber();
+        //        현재 얼마있는 조회
+        long current = ba.getBalance();
+        System.out.println("이름은 " + name + " 계좌번호는 " + accountNumber + " 금액은 " + current);
+    };
+
+    public void deposit(long money, BankAccount ba){
+//        현재 얼마있는 조회
+        long current = ba.getBalance();
+//        그 금액과 money를 합산하여 새로운 balance update
+        long newBalance = current + money;
+        ba.updateBalance(newBalance);
+    };
+
+    public boolean withdraw(long money, BankAccount ba){
+//        현재 얼마있는 조회 && 가능한 잔고인지 체크
+        long current = ba.getBalance();
+        if(current < money){
+            return false;
+        }else {
+//        그 금액에서 money를 차감하여 새로운 balance update
+            long newBalance = current - money;
+            ba.updateBalance(newBalance);
+            return true;
+        }
+    }
+//    송금
+    public boolean transfer(long money, BankAccount ba, BankAccount ba2){
+//        현재 얼마있는 조회 && 가능한 잔고인지 체크
+        long current = ba.getBalance();
+        long current2 = ba2.getBalance();
+        if(current < money){
+            return false;
+        }else {
+//        그 금액에서 money를 차감하여 새로운 balance update
+            long newBalance = current - money;
+            ba.updateBalance(newBalance);
+            long newBalance2 = current2 + money;
+            ba2.updateBalance(newBalance2);
+            return true;
+        }
+    }
 }
